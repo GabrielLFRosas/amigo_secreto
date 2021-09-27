158015e490bba6e25d28969ba302e7d10293ccaf
GabrielLFRosas <gabriel.rosas2k@gmail.com>
2021-09-27 16:43:43 -0300

    login

---
+++ api/auth.js
@@ -0,0 +1,25 @@
+module.exports = app => {
+
+    const signin = async (req, res) => {
+        if(!req.body.email || !req.body.name) return res.status(400).send("Informe um nome/email")
+
+        const user = await app.db('users')
+                                .where({ email: req.body.email})
+                                .first()
+
+        if(!user) {
+            app.db('users')
+                .insert(user)
+                .then( _ => res.status(201).send())
+                .catch( err => res.status(400).status( err ))
+
+            //return res.status(400).send("Usuario nao encontrado!")
+        } else {
+            return res.status(200).send()
+            //console.log("logou")
+        }
+
+    }
+
+    return { signin }
+}
--- migrations/20210924182506_create_table_users.js
+++ migrations/20210924182506_create_table_users.js
@@ -3,7 +3,7 @@ exports.up = function(knex) {
   return knex.schema.createTable('users', table => {
       table.increments('id').primary()
       table.string('name').notNull()
-      table.string('email').notNull().unique()
+      table.string('email').notNull()
       table.integer('group_id').references('id')
             .inTable('groups')
       table.integer('friend_id')
